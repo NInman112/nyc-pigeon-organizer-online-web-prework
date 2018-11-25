c29828509cf25534bfce73fff795de16bba4003d
Nick Inman <nick@theoatmeal.com>
2018-11-25 04:08:32 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,16 @@ def nyc_pigeon_organizer(data)
   
   pigeon_list = {}
   
-  
+
+  data.each do |value, data|
+    datas.each do |language, type|
+      if new_hash[language]
+        new_hash[language][:style] << value
+      else 
+        new_hash[language] = type
+        new_hash[language][:style] = [value]
+      end
+    end
+  end
+  new_hash
 end
