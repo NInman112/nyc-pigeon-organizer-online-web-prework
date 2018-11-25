e4e451e9c03422a76ee1ff078ff11ecccf67aca7
Nick Inman <nick@theoatmeal.com>
2018-11-25 04:11:43 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,10 @@
 require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
-  
   pigeon_list = {}
   
-
   data.each do |value, datas|
-    datas.each do |language, type|
-      if pigeon_list[language]
-        binding.pry
-        pigeon_list[language][:style] << value
-      else 
-        pigeon_list[language] = type
-        pigeon_list[language][:style] = [value]
-      end
-    end
+  binding.pry
   end
   pigeon_list
 end
