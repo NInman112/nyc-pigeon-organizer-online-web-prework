0ce8d6d2176e2b3a1ef14516fa55c18c327fb1b8
Nick Inman <nick@theoatmeal.com>
2018-11-25 04:15:26 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,10 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   
-  data.each do |value, datas|
-  binding.pry
+  data.each do |value, info|
+    info.each do |color, name|
+        binding.pry
+    end
   end
   pigeon_list
 end
