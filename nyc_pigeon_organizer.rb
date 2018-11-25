413eccdc67d743829edca24bcb93602261dca9de
Nick Inman <nick@theoatmeal.com>
2018-11-25 04:32:08 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   data.each do |value, info|
     info.each do |color, name|
       if pigeon_list[color]
-      pigeon_list[name] = name.flatten
+      pigeon_list[] = name.flatten
     else
       pigeon_list[color] = name
     end
